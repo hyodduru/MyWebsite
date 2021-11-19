4e6a9177f47c938095fa7603ee521215453e9681
hyodduru <hoje15v@gmail.com>
2021-11-19 20:50:30 +0900

    CSS : scroll to section

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 // transparent navbar
-const navbar= document.querySelector('.navbar')
+const navbar= document.querySelector('#navbar')
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 window.addEventListener('scroll',()=>{
@@ -10,3 +10,18 @@ window.addEventListener('scroll',()=>{
         navbar.classList.remove('active');
     }
 })
+
+// scroll to section
+const navbarMenu = document.querySelector('.navbar__menu');
+const homeBtn = document.querySelector('.home__btn');
+navbarMenu.addEventListener('click',scrollToSection);
+homeBtn.addEventListener('click', scrollToSection);
+
+function scrollToSection(event){
+    const id = event.target.dataset.id;
+    if(id==null){
+        return;
+    }
+    const section = document.querySelector(`#${id}`);
+    section.scrollIntoView({behavior :"smooth"})
+}
