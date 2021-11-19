f8c73b4f654704b278fac38b3b41bf468c925a6d
hyodduru <hoje15v@gmail.com>
2021-11-19 21:59:47 +0900

    JS : arrow-up btn

--- main.js
+++ main.js
@@ -14,26 +14,47 @@ window.addEventListener('scroll',()=>{
 // scroll to section
 const navbarMenu = document.querySelector('.navbar__menu');
 const homeBtn = document.querySelector('.home__btn');
-navbarMenu.addEventListener('click',scrollToSection);
-homeBtn.addEventListener('click', scrollToSection);
-
-function scrollToSection(event){
+navbarMenu.addEventListener('click',(event)=>{
     const id = event.target.dataset.id;
     if(id==null){
         return;
     }
-    const section = document.querySelector(`#${id}`);
-    section.scrollIntoView({behavior :"smooth"})
-}
+    scrollIntoView(`#${id}`)
+});
+
+homeBtn.addEventListener('click', ()=>{
+    scrollIntoView('#contact');})
+
 
 //transparent home : Make home slowly fade to transparent as the window scrolls down
-const home = document.querySelector('#home');
+const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 
 window.addEventListener('scroll',()=>{
     if(window.scrollY<homeHeight){
-       console.log(home.style.opacity = 1 - window.scrollY/homeHeight);
+       home.style.opacity = 1 - window.scrollY/homeHeight;
     }
 })
 
-//handle arrow-up btn
+//handle arrow-up btn : make arrow-up btn visible at the window scrolls down
+const arrowBtn = document.querySelector('.arrow-up');
+
+window.addEventListener('scroll',()=>{
+    if(window.scrollY>homeHeight/2){
+        arrowBtn.classList.add('visible');
+    }else{
+        arrowBtn.classList.remove('visible');
+    }
+})
+
+
+//scroll to home as the arrow-up btn is clicked
+arrowBtn.addEventListener('click', ()=>{
+    scrollIntoView('#home')})
+
+
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector) 
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+}
+
